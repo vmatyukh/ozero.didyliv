c9dde60859471f2c0401525d31b59240f96de460
Volodymyr Matyukh <volodymyr.matyukh.dev@gmail.com>
2026-05-17 00:48:54 +0300

    add payment method to float admin

---
+++ config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCog2IZVk-ypYEEAPs3XtQ85d5PPJkq5P0",
+  authDomain: "didyliv-be39d.firebaseapp.com",
+  databaseURL: "https://didyliv-be39d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "didyliv-be39d",
+  storageBucket: "didyliv-be39d.firebasestorage.app",
+  messagingSenderId: "507495924179",
+  appId: "1:507495924179:web:aa31425b25a024ba7def26"
+};
+var TG_BOT_TOKEN = "8225797469:AAGvYOw0_UlJhKBivzV1tcQ1-4-Odd7Fnig";
+var TG_ADMIN_IDS = "8459515447,592212598".split(',');
